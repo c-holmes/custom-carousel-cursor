e9f5092396130cd00758802f755cbf1aee08ee9f
Chris Holmes <chrisrobholmes@gmail.com>
2020-05-25 15:39:15 -0400

    Updates

---
+++ precache-manifest.0847ccab7d9147ebf304486c0eed7444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dcfda4088db96304cc09b2e4923cebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cd358a5806cedadf6a2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44d2d084f758730950e0",
+    "url": "/static/js/2.58c543f6.chunk.js"
+  },
+  {
+    "revision": "e26031d138ff8b848553a2183d3fd3f6",
+    "url": "/static/js/2.58c543f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd358a5806cedadf6a2",
+    "url": "/static/js/main.339a074e.chunk.js"
+  },
+  {
+    "revision": "5b81f43b4c1195563fe2",
+    "url": "/static/js/runtime-main.9b2441ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
